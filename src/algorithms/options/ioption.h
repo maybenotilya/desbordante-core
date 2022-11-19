754348650078aaa99c5fcee26240c998ad7e76ea
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-11-19 15:51:51 +0300

    Add IOption

---
+++ src/algorithms/options/ioption.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+#include <string_view>
+
+#include "boost/any.hpp"
+
+namespace algos::config {
+
+struct IOption {
+    virtual void SetDefault() = 0;
+    virtual void SetAny(boost::any value) = 0;
+    virtual void Unset() = 0;
+    [[nodiscard]] virtual bool IsSet() const = 0;
+    [[nodiscard]] virtual std::string_view GetName() const = 0;
+    virtual ~IOption() = default;
+};
+
+}  // namespace algos::config
+
---
+++ src/algorithms/options/option.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ioption.h"
+
+namespace algos::config {
+template <typename T>
+struct Option : IOption {
+
+};
+}  // namespace algos::config
