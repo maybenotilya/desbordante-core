2797d5eab58d102f471d57fd307a782a91a27719
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-05 16:12:24 +0300

    save point

--- src/core/algorithms/md/hymd/lattice_traverser.cpp
+++ src/core/algorithms/md/hymd/lattice_traverser.cpp
@@ -19,37 +19,37 @@ bool LatticeTraverser::TraverseLattice(bool traverse_all) {
             min_picker_lattice.Advance();
             continue;
         }
-        std::vector<model::LatticeMd> mds_to_add;
         std::vector<model::LatticeMd> mds_to_add_if_min;
         for (model::MdLatticeNodeInfo& node : cur) {
             model::SimilarityVector const& lhs_sims = node.lhs_sims;
-            model::SimilarityVector rhs_sims = *node.rhs_sims;
-            lattice.RemoveNode(lhs_sims);
+            model::SimilarityVector& rhs_sims = *node.rhs_sims;
+            model::SimilarityVector old_rhs_sims = rhs_sims;
+            rhs_sims.assign(col_matches_num, 0.0);
             model::SimilarityVector gen_max_rhs = lattice.GetMaxValidGeneralizationRhs(lhs_sims);
             auto [new_rhs_sims, is_unsupported] =
                     similarity_data.GetMaxRhsDecBounds(lhs_sims, recommendations_ptr_, min_support_,
-                                                       rhs_sims, gen_max_rhs);
+                                                       old_rhs_sims, gen_max_rhs);
             if (is_unsupported) {
                 lattice.MarkUnsupported(lhs_sims);
                 continue;
             }
             assert(new_rhs_sims.size() == col_matches_num);
-            for (size_t i = 0; i < new_rhs_sims.size(); ++i) {
+            for (size_t i = 0; i < col_matches_num; ++i) {
                 if (prune_nondisjoint_ && lhs_sims[i] != 0.0) continue;
                 model::Similarity const new_rhs_sim = new_rhs_sims[i];
                 if (new_rhs_sim > lhs_sims[i] && new_rhs_sim >= rhs_min_similarities[i] &&
                     new_rhs_sim > gen_max_rhs[i]) {
-                    mds_to_add.emplace_back(lhs_sims, new_rhs_sim, i);
+                    rhs_sims[i] = new_rhs_sim;
                 }
             }
             for (size_t i = 0; i < col_matches_num; ++i) {
                 std::optional<model::SimilarityVector> new_lhs_sims =
-                        similarity_data.SpecializeLhs(node.lhs_sims, i);
+                        similarity_data.SpecializeLhs(lhs_sims, i);
                 if (!new_lhs_sims.has_value()) continue;
                 if (lattice.IsUnsupported(new_lhs_sims.value())) continue;
                 for (size_t j = 0; j < col_matches_num; ++j) {
                     model::Similarity const new_lhs_sim = new_lhs_sims.value()[j];
-                    model::Similarity const rhs_sim = rhs_sims[j];
+                    model::Similarity const rhs_sim = old_rhs_sims[j];
                     if (prune_nondisjoint_ && new_lhs_sim != 0.0) continue;
                     if (rhs_sim > new_lhs_sim) {
                         mds_to_add_if_min.emplace_back(new_lhs_sims.value(), rhs_sim, j);
@@ -57,14 +57,10 @@ bool LatticeTraverser::TraverseLattice(bool traverse_all) {
                 }
             }
         }
-        for (auto& md : mds_to_add) {
-            lattice.Add(md);
-        }
         for (auto& md : mds_to_add_if_min) {
             lattice.AddIfMinAndNotUnsupported(md);
         }
         if (!traverse_all) return false;
-        // TODO: stop here.
         // TODO: if we specialized no LHSs, we can cut off the rest of the lattice here.
     }
     return true;
