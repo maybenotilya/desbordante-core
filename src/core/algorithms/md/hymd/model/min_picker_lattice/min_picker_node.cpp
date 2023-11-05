35d836d38fe121744a3fe1b9f1ac541382043dc8
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-05 16:25:44 +0300

    save point

--- src/core/algorithms/md/hymd/model/min_picker_lattice/min_picker_node.cpp
+++ src/core/algorithms/md/hymd/model/min_picker_lattice/min_picker_node.cpp
@@ -15,9 +15,8 @@ void MinPickerNode::Add(MdLatticeNodeInfo const& md, size_t this_node_index) {
         return;
     }
     assert(first_non_zero_index < lhs_vec.size());
-    size_t const child_array_index = first_non_zero_index - this_node_index;
     model::Similarity const child_similarity = lhs_vec[first_non_zero_index];
-    ThresholdMap& threshold_map = children_[child_array_index];
+    ThresholdMap& threshold_map = children_[first_non_zero_index];
     std::unique_ptr<MinPickerNode>& node_ptr = threshold_map[child_similarity];
     if (node_ptr == nullptr) {
         node_ptr = std::make_unique<MinPickerNode>();
@@ -29,16 +28,13 @@ bool MinPickerNode::HasGeneralization(MdLatticeNodeInfo const& md, size_t this_n
     if (IsMd()) return true;
     SimilarityVector const& lhs_vec = md.lhs_sims;
     for (auto const& [index, threshold_mapping] : children_) {
-        size_t const cur_node_index = this_node_index + index;
+        size_t const cur_node_index = index;
         assert(cur_node_index < lhs_vec.size());
+        Similarity const cur_lhs_sim = lhs_vec[index];
         for (auto const& [threshold, node] : threshold_mapping) {
             assert(threshold > 0.0);
-            if (threshold > lhs_vec[cur_node_index]) {
-                break;
-            }
-            if (node->HasGeneralization(md, cur_node_index + 1)) {
-                return true;
-            }
+            if (threshold > cur_lhs_sim) break;
+            if (node->HasGeneralization(md, cur_node_index + 1)) return true;
         }
     }
     return false;
@@ -55,14 +51,13 @@ void MinPickerNode::RemoveSpecializations(MdLatticeNodeInfo const& md, size_t th
     // if (IsMD()) { rhs_.clear(); return; }
     SimilarityVector const& lhs_vec = md.lhs_sims;
     for (auto const& [index, threshold_mapping] : children_) {
-        size_t const cur_node_index = this_node_index + index;
         assert(cur_node_index < lhs_vec.size());
-        double const next_node_sim = lhs_vec[cur_node_index];
+        double const next_node_sim = lhs_vec[index];
         if (next_node_sim == 0.0) continue;
         for (auto const& [threshold, node] : threshold_mapping) {
             assert(threshold > 0.0);
             if (threshold < next_node_sim) continue;
-            node->RemoveSpecializations(md, cur_node_index + 1);
+            node->RemoveSpecializations(md, index + 1);
         }
     }
     rhs_ = nullptr;
@@ -76,14 +71,14 @@ void MinPickerNode::GetAll(std::vector<MdLatticeNodeInfo>& collected,
         return;
     }
     for (auto const& [index, threshold_mapping] : children_) {
-        size_t const cur_node_index = this_node_index + index;
         assert(cur_node_index < this_node_lhs.size());
+        Similarity& cur_lhs_sim = this_node_lhs[index];
         for (auto const& [threshold, node] : threshold_mapping) {
             assert(threshold > 0.0);
-            this_node_lhs[cur_node_index] = threshold;
-            node->GetAll(collected, this_node_lhs, cur_node_index + 1, sims_left - 1);
+            cur_lhs_sim = threshold;
+            node->GetAll(collected, this_node_lhs, index + 1, sims_left - 1);
         }
-        this_node_lhs[cur_node_index] = 0.0;
+        cur_lhs_sim = 0.0;
     }
 }
 
