360a966cf41c0e223742e5191955d01f166a5df9
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-12-05 21:43:56 +0300

    Move functions to RhsTask

--- src/core/algorithms/md/hymd/lattice_traverser.cpp
+++ src/core/algorithms/md/hymd/lattice_traverser.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include "model/index.h"
+#include "util/parallel_for.h"
 
 namespace algos::hymd {
 
@@ -21,9 +22,103 @@ public:
     }
 };
 
+class RhsTask {
+    SimilarityData::ValidationResult result_;
+    lattice::FullLattice& lattice_;
+    lattice::ValidationInfo& info_;
+    SimilarityData& similarity_data_;
+    std::size_t min_support_;
+    Recommendations& recommendations_;
+    bool const prune_nondisjoint_;
+
+public:
+    RhsTask(lattice::FullLattice& lattice, lattice::ValidationInfo& info,
+            SimilarityData& similarity_data, std::size_t min_support,
+            Recommendations& recommendations, bool prune_nondisjoint)
+        : lattice_(lattice),
+          info_(info),
+          similarity_data_(similarity_data),
+          min_support_(min_support),
+          recommendations_(recommendations),
+          prune_nondisjoint_(prune_nondisjoint) {}
+
+    void Validate() {
+        result_ = similarity_data_.Validate(lattice_, info_, min_support_);
+    }
+
+    void AddViolations() {
+        for (auto const& rhs_violations : result_.violations) {
+            recommendations_.insert(rhs_violations.begin(), rhs_violations.end());
+        }
+    }
+
+    void Specialize() {
+        auto const& [violations, to_specialize, is_unsupported] = result_;
+        if (is_unsupported) {
+            lattice_.MarkUnsupported(info_.info->lhs_sims);
+            return;
+        }
+        DecisionBoundaryVector& lhs_sims = info_.info->lhs_sims;
+        std::size_t const col_matches_num = lhs_sims.size();
+        if (prune_nondisjoint_) {
+            for (model::Index lhs_spec_index = 0; lhs_spec_index < col_matches_num;
+                 ++lhs_spec_index) {
+                model::md::DecisionBoundary& lhs_sim = lhs_sims[lhs_spec_index];
+                std::optional<model::md::DecisionBoundary> const new_sim =
+                        similarity_data_.SpecializeOneLhs(lhs_spec_index, lhs_sim);
+                if (!new_sim.has_value()) continue;
+                auto context = SetForScope(lhs_sim, *new_sim);
+                if (lattice_.IsUnsupported(lhs_sims)) {
+                    continue;
+                }
+                auto it = to_specialize.begin();
+                auto end = to_specialize.end();
+                for (; it != end; ++it) {
+                    auto const& [index, old_bound] = *it;
+                    if (index == lhs_spec_index) {
+                        for (++it; it != end; ++it) {
+                            auto const& [index, old_bound] = *it;
+                            lattice_.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
+                        }
+                        break;
+                    }
+                    lattice_.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
+                }
+            }
+        } else {
+            for (model::Index lhs_spec_index = 0; lhs_spec_index < col_matches_num;
+                 ++lhs_spec_index) {
+                model::md::DecisionBoundary& lhs_sim = lhs_sims[lhs_spec_index];
+                std::optional<model::md::DecisionBoundary> const new_sim =
+                        similarity_data_.SpecializeOneLhs(lhs_spec_index, lhs_sim);
+                if (!new_sim.has_value()) continue;
+                auto context = SetForScope(lhs_sim, *new_sim);
+                if (lattice_.IsUnsupported(lhs_sims)) {
+                    continue;
+                }
+                auto it = to_specialize.begin();
+                auto end = to_specialize.end();
+                for (; it != end; ++it) {
+                    auto const& [index, old_bound] = *it;
+                    if (index == lhs_spec_index) {
+                        if (old_bound > *new_sim) {
+                            lattice_.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
+                        }
+                        for (++it; it != end; ++it) {
+                            auto const& [index, old_bound] = *it;
+                            lattice_.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
+                        }
+                        break;
+                    }
+                    lattice_.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
+                }
+            }
+        }
+    }
+};
+
 bool LatticeTraverser::TraverseLattice(bool traverse_all) {
     SimilarityData& similarity_data = *similarity_data_;
-    size_t const col_matches_num = similarity_data.GetColumnMatchNumber();
     lattice::FullLattice& lattice = *lattice_;
     lattice::LevelGetter& level_getter = *level_getter_;
     while (level_getter.AreLevelsLeft()) {
@@ -33,74 +128,14 @@ bool LatticeTraverser::TraverseLattice(bool traverse_all) {
         }
 
         for (lattice::ValidationInfo& info : mds) {
-            auto [violations, to_specialize, is_unsupported] =
-                    similarity_data.Validate(lattice, info, min_support_);
-            DecisionBoundaryVector& lhs_sims = info.info->lhs_sims;
-            for (auto const& rhs_violations : violations) {
-                recommendations_ptr_->insert(rhs_violations.begin(), rhs_violations.end());
-            }
-            // The above loop can be done concurrently with either of the below.
-            if (is_unsupported) {
-                lattice.MarkUnsupported(lhs_sims);
-                continue;
-            }
-            if (prune_nondisjoint_) {
-                for (model::Index lhs_spec_index = 0; lhs_spec_index < col_matches_num;
-                     ++lhs_spec_index) {
-                    model::md::DecisionBoundary& lhs_sim = lhs_sims[lhs_spec_index];
-                    std::optional<model::md::DecisionBoundary> const new_sim =
-                            similarity_data.SpecializeOneLhs(lhs_spec_index, lhs_sim);
-                    if (!new_sim.has_value()) continue;
-                    auto context = SetForScope(lhs_sim, *new_sim);
-                    if (lattice.IsUnsupported(lhs_sims)) {
-                        continue;
-                    }
-                    auto it = to_specialize.begin();
-                    auto end = to_specialize.end();
-                    for (; it != end; ++it) {
-                        auto const& [index, old_bound] = *it;
-                        if (index == lhs_spec_index) {
-                            for (++it; it != end; ++it) {
-                                auto const& [index, old_bound] = *it;
-                                lattice.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
-                            }
-                            break;
-                        }
-                        lattice.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
-                    }
-                }
-            } else {
-                for (model::Index lhs_spec_index = 0; lhs_spec_index < col_matches_num;
-                     ++lhs_spec_index) {
-                    model::md::DecisionBoundary& lhs_sim = lhs_sims[lhs_spec_index];
-                    std::optional<model::md::DecisionBoundary> const new_sim =
-                            similarity_data.SpecializeOneLhs(lhs_spec_index, lhs_sim);
-                    if (!new_sim.has_value()) continue;
-                    auto context = SetForScope(lhs_sim, *new_sim);
-                    if (lattice.IsUnsupported(lhs_sims)) {
-                        continue;
-                    }
-                    auto it = to_specialize.begin();
-                    auto end = to_specialize.end();
-                    for (; it != end; ++it) {
-                        auto const& [index, old_bound] = *it;
-                        if (index == lhs_spec_index) {
-                            if (old_bound > *new_sim) {
-                                lattice.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
-                            }
-                            for (++it; it != end; ++it) {
-                                auto const& [index, old_bound] = *it;
-                                lattice.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
-                            }
-                            break;
-                        }
-                        lattice.AddIfMinimalAndNotUnsupported(lhs_sims, old_bound, index);
-                    }
-                }
-            }
+            RhsTask task(lattice, info, similarity_data, min_support_, *recommendations_ptr_,
+                         prune_nondisjoint_);
+            task.Validate();
+            task.AddViolations();
+            task.Specialize();
         }
         if (!traverse_all) return false;
-        // TODO: if we specialized no LHSs, we can cut off the rest of the lattice here.
+        // TODO: if we specialized no LHSs, we can cut off the rest of the lattice here. (Can we?)
     }
     return true;
 }
--- src/core/util/parallel_for.h
+++ src/core/util/parallel_for.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <future>
 #include <system_error>
 #include <thread>
 #include <vector>
@@ -55,6 +56,51 @@ inline void ParallelForeach(It begin, It end, unsigned const threads_num_max, Un
 
     for (auto& thread : threads) {
         thread.join();
+    }
+}
+
+template <typename It, typename UnaryFunction>
+inline void parallel_foreach_async(It begin, It end, unsigned const tasks_num_max,
+                                   UnaryFunction f) {
+    assert(threads_num_max != 0);
+    auto const length = std::distance(begin, end);
+    if (length == 0) {
+        return;
+    }
+    auto const threads_num_actual =
+            static_cast<unsigned>(std::min(length, static_cast<decltype(length)>(tasks_num_max)));
+    auto const items_per_thread = std::distance(begin, end) / threads_num_actual;
+    std::vector<std::future<void>> tasks;
+    tasks.reserve(threads_num_actual);
+
+    auto const task = [&f](It first, It last) {
+        for (; first != last; ++first) {
+            f(*first);
+        }
+    };
+
+    It p = begin;
+    for (unsigned i = 0; i < threads_num_actual - 1; ++i) {
+        It prev = p;
+        std::advance(p, items_per_thread);
+        try {
+            tasks.emplace_back(task, prev, p);
+        } catch (std::system_error const& e) {
+            /* Could not create a new thread */
+            LOG(WARNING) << "Created " << tasks.size() << " tasks in parallel_foreach_async. "
+                         << "Could not create new task: " << e.what();
+            /* Fall through to the serial case for the remaining elements */
+            p = prev;
+            break;
+        }
+    }
+
+    for (; p != end; ++p) {
+        f(*p);
+    }
+
+    for (auto& task : tasks) {
+        task.get();
     }
 }
 
