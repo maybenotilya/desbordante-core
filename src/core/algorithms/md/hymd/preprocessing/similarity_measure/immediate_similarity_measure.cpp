5a154a9d5e9305b540462dfd341229256b16d767
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-12-11 20:15:34 +0300

    Paralellize similarity calculation

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/immediate_similarity_measure.cpp
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/immediate_similarity_measure.cpp
@@ -1,6 +1,7 @@
 #include "algorithms/md/hymd/preprocessing/similarity_measure/immediate_similarity_measure.h"
 
 #include "config/exceptions.h"
+#include "util/parallel_for.h"
 
 namespace algos::hymd::preprocessing::similarity_measure {
 
@@ -27,27 +28,50 @@ Similarity GetSimilarity(DataInfo const& data_info_left, DataInfo const& data_in
             compute_similarity(value_left, value_right).get());
 }
 
-indexes::ColumnSimilarityInfo ImmediateSimilarityMeasure::MakeIndexes(
-        std::shared_ptr<DataInfo const> data_info_left,
-        std::shared_ptr<DataInfo const> data_info_right,
-        std::vector<indexes::PliCluster> const* clusters_right, model::md::DecisionBoundary min_sim,
-        bool is_null_equal_null) const {
-    std::vector<model::md::DecisionBoundary> decision_bounds;
-    indexes::SimilarityMatrix similarity_matrix;
-    indexes::SimilarityIndex similarity_index;
-    auto const& data_left_size = data_info_left->GetElementNumber();
-    auto const& data_right_size = data_info_right->GetElementNumber();
-    Similarity lowest = 1.0;
-    for (ValueIdentifier value_id_left = 0; value_id_left < data_left_size; ++value_id_left) {
+struct SimCalcTask {
+    std::shared_ptr<DataInfo const> const& data_info_left;
+    std::shared_ptr<DataInfo const> const& data_info_right;
+    std::vector<indexes::PliCluster> const* clusters_right;
+    model::md::DecisionBoundary const min_sim;
+    bool const is_null_equal_null;
+    std::size_t const value_id_left;
+    SimilarityFunction const& compute_similarity;
+    indexes::SimilarityMatrixRow row{};
+    std::vector<model::md::DecisionBoundary> row_decision_bounds{};
+    indexes::SimInfo sim_info{};
+    model::md::DecisionBoundary row_lowest = 1.0;
+
+    Similarity GetSimilarity(DataInfo const& data_info_left, DataInfo const& data_info_right,
+                             SimilarityFunction const& compute_similarity, bool is_null_equal_null,
+                             ValueIdentifier value_id_left, ValueIdentifier value_id_right) {
+        auto const& left_data_info = data_info_left;
+        auto const& right_data_info = data_info_right;
+        auto const& left_nulls = left_data_info.GetNulls();
+        auto const& right_nulls = right_data_info.GetNulls();
+        auto const& left_empty = left_data_info.GetEmpty();
+        auto const& right_empty = right_data_info.GetEmpty();
+        if (left_nulls.find(value_id_left) != left_nulls.end()) {
+            if (is_null_equal_null && right_nulls.find(value_id_right) != right_nulls.end())
+                return 1.0;
+            return 0.0;
+        }
+        if (left_empty.find(value_id_left) != left_empty.end()) {
+            if (right_empty.find(value_id_right) != right_empty.end()) return 1.0;
+            return 0.0;
+        }
+        std::byte const* value_left = left_data_info.GetAt(value_id_left);
+        std::byte const* value_right = right_data_info.GetAt(value_id_right);
+        return model::Type::GetValue<model::md::DecisionBoundary>(
+                compute_similarity(value_left, value_right).get());
+    }
+
+    void MakeRow() {
         std::vector<std::pair<Similarity, RecordIdentifier>> sim_rec_id_vec;
-        indexes::SimilarityMatrixRow row;
-        std::vector<model::md::DecisionBoundary> row_decision_bounds;
-        indexes::SimInfo sim_info;
-        model::md::DecisionBoundary row_lowest = 1.0;
+        std::size_t const data_right_size = data_info_right->GetElementNumber();
         for (ValueIdentifier value_id_right = 0; value_id_right < data_right_size;
              ++value_id_right) {
             Similarity similarity =
-                    GetSimilarity(*data_info_left, *data_info_right, compute_similarity_,
+                    GetSimilarity(*data_info_left, *data_info_right, compute_similarity,
                                   is_null_equal_null, value_id_left, value_id_right);
             if (similarity < min_sim) {
                 // Metanome keeps the actual value for some reason.
@@ -61,10 +85,7 @@ indexes::ColumnSimilarityInfo ImmediateSimilarityMeasure::MakeIndexes(
                 sim_rec_id_vec.emplace_back(similarity, record_id);
             }
         }
-        if (sim_rec_id_vec.empty()) continue;
-        if (row_lowest < lowest) lowest = row_lowest;
-        decision_bounds.insert(decision_bounds.end(), row_decision_bounds.begin(),
-                               row_decision_bounds.end());
+        if (sim_rec_id_vec.empty()) return;
         std::sort(sim_rec_id_vec.begin(), sim_rec_id_vec.end(), std::greater<>{});
         std::size_t const rec_num = sim_rec_id_vec.size();
         std::vector<RecordIdentifier> records;
@@ -82,8 +103,34 @@ indexes::ColumnSimilarityInfo ImmediateSimilarityMeasure::MakeIndexes(
             previous_similarity = similarity;
         }
         sim_info[previous_similarity] = {it_begin, records.end()};
-        similarity_index[value_id_left] = std::move(sim_info);
-        similarity_matrix[value_id_left] = std::move(row);
+    }
+};
+
+indexes::ColumnSimilarityInfo ImmediateSimilarityMeasure::MakeIndexes(
+        std::shared_ptr<DataInfo const> data_info_left,
+        std::shared_ptr<DataInfo const> data_info_right,
+        std::vector<indexes::PliCluster> const* clusters_right, model::md::DecisionBoundary min_sim,
+        bool is_null_equal_null) const {
+    std::vector<model::md::DecisionBoundary> decision_bounds;
+    indexes::SimilarityMatrix similarity_matrix;
+    indexes::SimilarityIndex similarity_index;
+    auto const& data_left_size = data_info_left->GetElementNumber();
+    Similarity lowest = 1.0;
+    std::vector<SimCalcTask> tasks;
+    for (ValueIdentifier value_id_left = 0; value_id_left < data_left_size; ++value_id_left) {
+        tasks.emplace_back(data_info_left, data_info_right, clusters_right, min_sim,
+                           is_null_equal_null, value_id_left, compute_similarity_);
+    }
+    util::parallel_foreach(tasks.begin(), tasks.end(), 12, [](SimCalcTask& task) {
+        task.MakeRow();
+    });
+    for (SimCalcTask& task : tasks) {
+        if (task.row_decision_bounds.empty()) continue;
+        similarity_index[task.value_id_left] = std::move(task.sim_info);
+        similarity_matrix[task.value_id_left] = std::move(task.row);
+        decision_bounds.insert(decision_bounds.end(), task.row_decision_bounds.begin(),
+                               task.row_decision_bounds.end());
+        if (task.row_lowest < lowest) lowest = task.row_lowest;
     }
     std::sort(decision_bounds.begin(), decision_bounds.end());
     decision_bounds.erase(std::unique(decision_bounds.begin(), decision_bounds.end()),
