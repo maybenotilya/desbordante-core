6c0fd093e28029102c7ac003ab4da9fe78684904
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-12-18 22:44:46 +0300

    comparison fix

--- src/core/algorithms/md/hymd/lattice/cardinality/one_by_one_min_picker.cpp
+++ src/core/algorithms/md/hymd/lattice/cardinality/one_by_one_min_picker.cpp
@@ -18,8 +18,9 @@ void OneByOnePicker::AddGeneralizations(MdLatticeNodeInfo& node_info,
     for (ValidationInfo& prev_info : currently_picked_) {
         DecisionBoundaryVector const& lhs_bounds_prev = prev_info.node_info->lhs_bounds;
         boost::dynamic_bitset<>& indices_prev = prev_info.rhs_indices;
-        for (auto cur_it = cur_begin, prev_it = lhs_bounds_prev.begin(); cur_it != cur_end;
-             ++cur_it, ++prev_it) {
+        auto cur_it = cur_begin;
+        auto prev_it = lhs_bounds_prev.begin();
+        while (cur_it != cur_end) {
             model::md::DecisionBoundary const cur_bound = *cur_it;
             model::md::DecisionBoundary const prev_bound = *prev_it;
             if (cur_bound < prev_bound) {
@@ -31,6 +32,7 @@ void OneByOnePicker::AddGeneralizations(MdLatticeNodeInfo& node_info,
                     }
                 }
                 indices_prev -= considered_indices;
+                break;
             } else if (cur_bound > prev_bound) {
                 for (++cur_it, ++prev_it; cur_it != cur_end; ++cur_it, ++prev_it) {
                     model::md::DecisionBoundary const cur_bound = *cur_it;
@@ -41,6 +43,10 @@ void OneByOnePicker::AddGeneralizations(MdLatticeNodeInfo& node_info,
                 }
                 considered_indices -= indices_prev;
                 if (considered_indices.none()) return;
+                break;
+            } else {
+                ++cur_it;
+                ++prev_it;
             }
         }
     incomparable:;
