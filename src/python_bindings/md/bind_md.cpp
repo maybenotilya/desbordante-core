3b6a82e51a691b6e26d1d921af5c8015954fceae
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-02-17 15:28:43 +0300

    Python support

--- src/python_bindings/md/bind_md.cpp
+++ src/python_bindings/md/bind_md.cpp
@@ -3,19 +3,23 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_similarity_measure.h"
+#include "algorithms/md/hymd/similarity_measure_creator.h"
 #include "algorithms/md/md.h"
 #include "algorithms/md/mining_algorithms.h"
 #include "py_util/bind_primitive.h"
 
 namespace {
 namespace py = pybind11;
-using algos::hymd::HyMD;
 using model::MD;
 }  // namespace
 
 namespace python_bindings {
 void BindMd(py::module_& main_module) {
     using namespace algos;
+    using namespace algos::hymd;
+    using namespace algos::hymd::preprocessing::similarity_measure;
+    using namespace py::literals;
 
     auto md_module = main_module.def_submodule("md");
     py::class_<MD>(md_module, "MD")
@@ -24,6 +28,14 @@ void BindMd(py::module_& main_module) {
             .def("to_long_string", &MD::ToStringFull)
             .def("to_short_string", &MD::ToStringShort)
             .def("__str__", &MD::ToStringShort);
-    BindPrimitive<hymd::HyMD>(md_module, &MdAlgorithm::MdList, "MdAlgorithm", "get_mds", {"HyMD"});
+    py::class_<SimilarityMeasureCreator, std::shared_ptr<SimilarityMeasureCreator>>(
+            md_module, "SimilarityMeasure");
+    py::class_<LevenshteinSimilarityMeasure::Creator, SimilarityMeasureCreator,
+               std::shared_ptr<LevenshteinSimilarityMeasure::Creator>>(md_module,
+                                                                       "LevenshteinSimilarity")
+            .def(py::init<model::md::DecisionBoundary, bool, std::size_t>(),
+                 "minimum_bound"_a = 0.7, "is_null_equal_null"_a = true,
+                 "bound_number_limit"_a = 0);
+    BindPrimitive<HyMD>(md_module, &MdAlgorithm::MdList, "MdAlgorithm", "get_mds", {"HyMD"});
 }
 }  // namespace python_bindings
