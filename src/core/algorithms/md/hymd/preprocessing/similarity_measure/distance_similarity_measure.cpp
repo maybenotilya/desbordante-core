a1807d66876804983b72c2ef21d37209d5005939
Daniil Nikolaev <quanttlight@gmail.com>
2024-05-07 02:37:56 +0300

    Fix unused thread_pool

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/distance_similarity_measure.cpp
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/distance_similarity_measure.cpp
@@ -16,7 +16,7 @@ indexes::SimilarityMeasureOutput DistanceSimilarityMeasure::MakeIndexes(
         std::shared_ptr<DataInfo const> data_info_left,
         std::shared_ptr<DataInfo const> data_info_right,
         std::vector<indexes::PliCluster> const& clusters_right,
-        util::WorkerThreadPool& thread_pool) const {
+        util::WorkerThreadPool&) const {
     std::vector<model::md::DecisionBoundary> decision_bounds;
     indexes::SimilarityMatrix similarity_matrix;
     indexes::SimilarityIndex similarity_index;
