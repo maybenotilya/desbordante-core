e86505b5a2ca2450006e9783fba9b53b66c17758
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-08-01 21:08:17 +0700

    misc fixes

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_distance.h
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_distance.h
@@ -1,3 +1,7 @@
+#pragma once
+
 #include <string_view>
 
+namespace algos::hymd::preprocessing {
 unsigned LevenshteinDistance(std::string_view l, std::string_view r) noexcept;
+}  // namespace algos::hymd::preprocessing
